80e411d90ab02261d43bbd588a8416149ec394d0
nitaryn <anrynsilonkoi@gmail.com>
2016-02-03 11:04:04 +0300

    asigning value and indention in python

---
+++ syntax_assigning values.py
@@ -0,0 +1,14 @@
+def main(): 
+	a,b= 3,4
+	print(a, b)
+	
+
+if __name__ == '__main__':main()  
+
+def main(): 
+	a= (3,4, 5, 6,7)
+	print (a)
+
+	
+
+if __name__ == '__main__':main()
--- syntax_whitespace.py
+++ syntax_whitespace.py
@@ -1,5 +1,5 @@
-def main():  #Take this into consideration while working with python always.
-	print("Indentation is important in python")
-	print("For python beginners")
+def main():
+	print("indention is important in python")
+	print("Always use similar indention space")
 
-if __name__ == '__main__':main()  #This must be placed at the end of every code in python.
+if __name__ == '__main__':main()
