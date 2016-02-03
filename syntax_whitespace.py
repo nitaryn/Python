b539f5c05c0034005bb2765bf1307ac9426fb8ce
nitaryn <anrynsilonkoi@gmail.com>
2016-02-03 10:39:21 +0300

    how to make comments in python

--- syntax_whitespace.py
+++ syntax_whitespace.py
@@ -1,5 +1,5 @@
-def main():
+def main():  #Take this into consideration while working with python always.
 	print("Indentation is important in python")
 	print("For python beginners")
 
-if __name__ == '__main__':main()
+if __name__ == '__main__':main()  #This must be placed at the end of every code in python.
