e43840e7ea81a9c6cd1d05d387d3e9bc8f8071a7
nitaryn <anrynsilonkoi@gmail.com>
2016-02-03 14:33:38 +0300

    using strings in python

---
+++ variable_strings.py
@@ -0,0 +1,16 @@
+def main():
+	n=77
+	s= "This is a {} string".format(n)
+	print(s)
+
+if __name__ == '__main__':main()
+
+def main():
+	s = '''\
+	This is a string
+	it has 77 lines of code
+	it is also defined'''
+	print(s)
+
+if __name__ == '__main__':main()
+
