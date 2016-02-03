64044ebf47fa3bea78ee078ef41623c0e67eb65a
nitaryn <anrynsilonkoi@gmail.com>
2016-02-03 13:01:28 +0300

    using numbers in python

---
+++ variable.py
@@ -0,0 +1,20 @@
+def main():
+	num = 44
+	print(num)
+
+if __name__ == '__main__':main()
+
+def main():
+	num = 44/4
+	print(type (num), num)
+
+if __name__ == '__main__':main()
+
+def main():
+	num =round(45//3,2)
+	print(type (num),num)
+
+if __name__ == '__main__':main()
+
+
+
