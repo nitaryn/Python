20038edd444fe7e1c8290f7f3ad1f6b9f259f648
nitaryn <anrynsilonkoi@gmail.com>
2016-02-03 15:50:42 +0300

    the elif condition

---
+++ conditional_elif.py
@@ -0,0 +1,13 @@
+def main():
+	v= "two"
+	if v == 'two':
+		print("v is two")
+
+    elif v == 'three':
+     	print ("v is three")
+    elif v == 'seven':
+     	print("v is seven"
+    else:
+     	print("v is something else")
+if __name__ == '__main__':
+	main()
