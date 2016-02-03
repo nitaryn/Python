3b43dfd28f42ca825c7848c92c7ed217f0f82801
nitaryn <anrynsilonkoi@gmail.com>
2016-02-03 11:11:58 +0300

    how to makecomments in python

---
+++ comments.py
@@ -0,0 +1,5 @@
+def main(): #The indention must be kept in consideration in python
+	print("indention is important in python")
+	print("Always use similar indention space")
+
+if __name__ == '__main__':main()  #This should be always at the end of the code
