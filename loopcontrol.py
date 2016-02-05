fa86d64d0ce8892a4bbab9111925c29065a2d662
nitaryn <anrynsilonkoi@gmail.com>
2016-02-05 07:53:26 +0300

    controlling loop flow with else statement

--- loopcontrol.py
+++ loopcontrol.py
@@ -5,3 +5,12 @@ def main():
 		print (c, end ='')
 if __name__ == '__main__':
 	main()
+
+def main():
+	s = 'this is a string'
+	for c in s:
+		print (c, end ='')
+	else:
+		print('else')
+if __name__ == '__main__':
+	main()
