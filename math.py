aa200bbbe3b0da212fea39d2519573935c40228b
nitaryn <anrynsilonkoi@gmail.com>
2016-02-10 10:38:19 +0300

    math operators in python

---
+++ math.py
@@ -0,0 +1,10 @@
+print"I will count my books:"
+
+print "Textbook",5+9/7
+print "Exercise books",500-250*10%5
+
+print "Now i will count the pens:"
+
+print 3+2+1-5+4%2-1/4+6
+
+print "is it true that 3+2<5-7?"
