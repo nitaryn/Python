a7693903920f181af2b6792f38f71537954ac73d
nitaryn <anrynsilonkoi@gmail.com>
2016-02-12 17:33:28 +0300

    using methods in classes

---
+++ classes2.py
@@ -0,0 +1,17 @@
+class Human:
+	def__init__ (self, value)
+		self._v = value
+
+    def eat(self):
+		print("eating food",self._v)
+
+	def talk(self):
+		print("Likes talking while eating",self._v)
+
+def main():
+	frank= Human(45)
+	frank.eat()
+	frank.talk()
+
+if __name__ == '__main__':
+	main()
