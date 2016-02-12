fbae1a14748c9ba129062426ad9a249880684fc1
nitaryn <anrynsilonkoi@gmail.com>
2016-02-12 18:11:51 +0300

    using object data

---
+++ classes3.py
@@ -0,0 +1,22 @@
+class Human:
+	def__init__ (self, sex ='male')
+		self._sex =  male
+
+    def eat(self):
+		print("eating food")
+
+	def talk(self):
+		print("Likes talking while eating")
+
+	def define_sex(self, sex):
+	    self._sex = sex
+
+	def get_sex(self):
+		return self._sex
+
+    def main():
+	    frank= Human()
+	    print(frank.get_sex())
+
+if __name__ == '__main__':
+	main()
